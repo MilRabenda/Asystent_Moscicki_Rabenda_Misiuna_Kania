f7843606503d6bf8b9ec8db6570ddb466e9ec6ec
marcmis979 <marcmis979@student.polsl.pl>
2022-05-23 19:39:06 +0200

    guestciście!

--- Guest1.xaml.cs
+++ Guest1.xaml.cs
@@ -26,8 +26,9 @@ namespace Panel_Gościa
     public partial class Guest1 : Page
     {
         public static int i = 0;
-        List<string> namesList;
         List<string> sourceList;
+        List<string> namesList; 
+        List<string> prizeList;
         DispatcherTimer dispatcherTimer = new DispatcherTimer(); //next image per 3s
         public Guest1()
         {
@@ -48,13 +49,18 @@ namespace Panel_Gościa
                 ImageFrame.Source = new BitmapImage(new Uri(sourceList[i], UriKind.Relative));
 
                 namesList = new List<string>();//names
+                prizeList = new List<string>();//prizes
                 for (int j = 1; j <= size; j++)
                 {
                     MySqlCommand commandName = new MySqlCommand($@"SELECT nazwabadania FROM badanie WHERE idbadania={j}", connect);
+                    MySqlCommand commandPrize = new MySqlCommand($@"SELECT cennik FROM badanie WHERE idbadania={j}", connect);
                     string name = Convert.ToString(commandName.ExecuteScalar());
+                    string prize = Convert.ToString(commandPrize.ExecuteScalar() +" zł");
                     namesList.Add(name);
+                    prizeList.Add(prize);
                 }
                 lblName.Content = namesList[i];
+                lblPrize.Content = prizeList[i];
                 connect.Close(); 
             }
             
@@ -66,6 +72,7 @@ namespace Panel_Gościa
         {
            i++; if (i > 6) i = 0;
            lblName.Content = namesList[i];
+           lblPrize.Content = prizeList[i];
            ImageFrame.Source = new BitmapImage(new Uri(sourceList[i], UriKind.Relative));
         }
 
@@ -80,7 +87,8 @@ namespace Panel_Gościa
             i--;
             if (i < 0) i = 6;
             ImageFrame.Source = new BitmapImage(new Uri(sourceList[i], UriKind.Relative));
-            lblName.Content= namesList[i];
+            lblName.Content = namesList[i];
+            lblPrize.Content = prizeList[i];
             dispatcherTimer.Stop();
             dispatcherTimer.Start();
         }
@@ -91,6 +99,7 @@ namespace Panel_Gościa
             if (i > 6) i = 0; 
             ImageFrame.Source = new BitmapImage(new Uri(sourceList[i], UriKind.Relative));
             lblName.Content = namesList[i];
+            lblPrize.Content = prizeList[i];
             dispatcherTimer.Stop();
             dispatcherTimer.Start();
         }
