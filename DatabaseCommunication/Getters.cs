6b37ddefa8527946da6df5de9c5ad986b4a10fc3
maciejmoscicki17 <macimos024@student.polsl.pl>
2022-05-23 20:39:13 +0200

    23.05
    database communication

---
+++ DatabaseCommunication/Getters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace DatabaseCommunication
+{
+    public static class Getters
+    {
+
+        public static List<string> getWizyty(int idPacjenta)
+        {
+            using (MySqlConnection połączenie = new MySqlConnection(@"server=localhost;user id=root; password=2137;database=laboratorium"))
+            {
+
+                MySqlCommand log = new MySqlCommand($@"select datawizyty from wizyta where idpacjenta={idPacjenta}", połączenie);
+                połączenie.Open();
+                MySqlDataReader reader = log.ExecuteReader();
+
+                List<string> lista = new List<string>();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var f = reader.GetString(0);
+                        lista.Add(f);
+                    }
+                }
+                reader.Close();
+                połączenie.Close();
+                return lista;
+
+            }
+        }
+    }
+}
