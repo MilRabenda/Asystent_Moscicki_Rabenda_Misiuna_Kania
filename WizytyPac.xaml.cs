79ec340334998fb50a0e2c3ab953ef6ce4f0be1b
MilRabenda <73226944+MilRabenda@users.noreply.github.com>
2022-06-02 18:29:40 +0200

    Wyświetlanie wizyt dla pacjenta:)

--- WizytyPac.xaml.cs
+++ WizytyPac.xaml.cs
@@ -20,6 +20,55 @@ namespace Panel_Gościa
     /// <summary>
     /// Logika interakcji dla klasy WizytyPac.xaml
     /// </summary>
+    /// 
+    public class Wizyta
+    {
+        public DateTime data;
+        public int idpielęgniarki;
+        public int idbadania;
+
+        public DateTime Data
+        {
+            set { this.data = value; }
+            get { return data; }
+        }
+
+        public int IdPielęgniarki
+        {
+            set { this.idpielęgniarki = value; }
+            get { return idpielęgniarki; }
+        }
+
+        public int IdBadania
+        {
+            set { this.idbadania = value; }
+            get { return idbadania; }
+        }
+
+        public override string ToString()
+        {
+            using (MySqlConnection połączenie = new MySqlConnection(Getters.connectionString))
+            {
+                MySqlCommand polecenie1  = new MySqlCommand($@"SELECT idosoby FROM pielegniarki where idpielegniarki={IdPielęgniarki}", połączenie);
+                połączenie.Open();
+                MySqlDataReader Czytaj = polecenie1.ExecuteReader();
+                Czytaj.Read();
+                int idp = Convert.ToInt32(Czytaj.GetValue(0));
+                Czytaj.Close();
+                MySqlCommand polecenie2 = new MySqlCommand($@"SELECT imie, nazwisko FROM osoba where idosoby={idp};", połączenie);
+                MySqlDataReader Czytaj2 = polecenie2.ExecuteReader();
+                Czytaj2.Read();
+                string imiepielegniarki=Convert.ToString(Czytaj2.GetValue(0)) + " " + Convert.ToString(Czytaj2.GetValue(1));
+                Czytaj2.Close();
+                MySqlCommand polecenie3 = new MySqlCommand($@"SELECT nazwabadania FROM badanie where idbadania={IdBadania};", połączenie);
+                MySqlDataReader Czytaj3 = polecenie3.ExecuteReader();
+                Czytaj3.Read();
+                string nazwabadania = Convert.ToString(Czytaj3.GetValue(0));
+                Czytaj3.Close();
+                return $@"{nazwabadania} | {Data} | {imiepielegniarki}";
+            }
+        }
+    }
     public partial class WizytyPac : Page
     {
         public int id;
@@ -28,18 +77,34 @@ namespace Panel_Gościa
             set { this.id = value; }
             get { return id; }
         }
+        List<Wizyta> listawizyt = new List<Wizyta>();
         public WizytyPac(int idpac)
         {
             ID = idpac;
             InitializeComponent();
+            WysWizyty();
             
         }
 
         public void WysWizyty()
         {
+            
             using (MySqlConnection połączenie = new MySqlConnection(Getters.connectionString))
             {
-                //pomysł był taki żeby zrobić w tym miejscu wyciąganie wizyt do listy. Wizyta będzie klasą. Tbc:)
+                MySqlCommand polecenie = new MySqlCommand($@"SELECT * FROM wizyta where idpacjenta={ID}", połączenie);
+                połączenie.Open();
+                MySqlDataReader Czytaj = polecenie.ExecuteReader();
+               while( Czytaj.Read())
+                {
+                    Wizyta w = new Wizyta();
+                    w.IdPielęgniarki = Convert.ToInt32(Czytaj.GetValue(1));
+                    w.IdBadania= Convert.ToInt32(Czytaj.GetValue(4));
+                    w.Data = Convert.ToDateTime(Czytaj.GetValue(3));
+                    listawizyt.Add(w);
+                    lbxWizyty.Items.Add(w.ToString());
+                }
+                Czytaj.Close();
+                połączenie.Close();
             }
         }
     }
